9520542a05a77d1bd9792bdd4617e1009d043075
evgeny.zibrov <evgeny.zibrov@gmail.com>
2012-10-16 16:13:24 +0300

    Add sidebar tests to static_pages_spec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,24 +3,46 @@ require 'spec_helper'
 describe "Static pages" do
   
 	describe "Home page" do
+
 		it "should have content 'Sample App'" do
 			visit '/static_pages/home'
-			page.should have_content('Sample App')
+			page.should have_selector('h1', :text =>'Sample App')
+			#page.should have_content('Sample App')
 		end
+
+		it "should have side bar" do
+			visit '/static_pages/home'
+		  	page.should have_selector('h3', text: 'Sidebar')
+		end
+
 	end
 
 	describe "Help page" do
+
 		it "should have content 'Help'" do
 			visit '/static_pages/help'
-			page.should have_content('Help')
+			page.should have_selector('h1', :text => 'Help')
 		end
+
+		it "should have side bar" do
+			visit '/static_pages/home'
+		    page.should have_selector('h3', text: 'Sidebar')
+		end
+
 	end
 
 	describe "About page" do
+
 		it "should have content 'About us'" do
 			visit '/static_pages/about'
-			page.should have_content('About us')
+			page.should have_selector('h1', :text =>'About us')
 		end
+
+		it "should have side bar" do
+			visit '/static_pages/home'
+		  	page.should have_selector('h3', text: 'Sidebar')
+		end
+
 	end
 
 end
